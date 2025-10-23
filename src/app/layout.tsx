377122629997cdeda347f543e3a29fb0171ea085
Abhisharma08 <aksharma223@gmail.com>
2025-10-23 16:33:19 +0530

    chatbot added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,25 @@ export default function RootLayout({
             gtag('config', '${GA_ID}');
           `}
         </Script>
+
+          <Script
+          id="tawk-to"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+                var s1=document.createElement("script"), s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/YOUR_PROPERTY_ID/default';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
