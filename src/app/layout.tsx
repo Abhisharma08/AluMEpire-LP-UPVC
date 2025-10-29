4d94530fd0287e92b0d3ad362374a502eb613d74
Abhisharma08 <aksharma223@gmail.com>
2025-10-29 13:07:41 +0530

    chatbot added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,23 +34,7 @@ export default function RootLayout({
           `}
         </Script>
 
-          <Script
-          id="tawk-to"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-              (function(){
-                var s1=document.createElement("script"), s0=document.getElementsByTagName("script")[0];
-                s1.async=true;
-                s1.src='https://embed.tawk.to/YOUR_PROPERTY_ID/default';
-                s1.charset='UTF-8';
-                s1.setAttribute('crossorigin','*');
-                s0.parentNode.insertBefore(s1,s0);
-              })();
-            `,
-          }}
-        />
+        <script src="//code.tidio.co/bdmtqpq4b4dqdre0zp8envvhzqjx6kye.js" async></script>
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
